acd3772b68d68832d878961f06ff6f41dcf2ff4a
muratsalakhov <muratsalakhov@gmail.com>
2021-04-12 23:01:24 +0300

    Program model update

--- app/Models/Program.php
+++ app/Models/Program.php
@@ -4,19 +4,35 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use phpDocumentor\Reflection\Types\Resource_;
 
-class Program extends Model
+class Program //extends Model
 {
-    use HasFactory;
+    //use HasFactory;
 
     protected $primaryKey;
     protected $images;
     protected $zip;
     protected $body;
 
-    public function __construct(string $url)
-    {
-
-
+    protected function getProgramByUrl(string $url) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        $this->zip = curl_exec($ch);
+        curl_close($ch);
     }
-}
+
+    public function unzipProgram() {
+        $zip = new ZipArchive;
+        if ($zip->open('/storage/app/public/zip/test.zip') === TRUE) {
+            $zip->extractTo('/storage/app/public/images', '*.png');
+            $zip->extractTo('/storage/app/public/json', '*.json');
+            $zip->close();
+            echo 'готово';
+        } else {
+            echo 'ошибка';
+        }
+    }
+};
